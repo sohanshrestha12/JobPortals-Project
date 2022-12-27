83e5daa8f10653bc180fec20d404b0e783419bfd
ujwaldai <ujwalshakha@gmail.com>
2022-12-27 21:54:53 +0545

    intital commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
-import path from "path";
+ import path from "path";
 
 export default defineConfig({
     plugins: [
@@ -13,9 +12,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
-        },
+    resolve: { 
+        alias: { 
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'), 
+        } 
     },
+
+
 });
